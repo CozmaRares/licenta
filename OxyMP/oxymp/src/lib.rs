2a6203159b886cc6c38dacdc9bcf93766d64adb0
Cozma Rares <cozmarares11@gmail.com>
2024-07-13 13:16:45 +0300

    feat: add macro attributes and group them

--- OxyMP/oxymp/src/lib.rs
+++ OxyMP/oxymp/src/lib.rs
@@ -1,12 +1,57 @@
-use proc_macro::{self, TokenStream};
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use std::collections::{HashMap, HashSet};
 
-#[proc_macro_derive(MyTrait)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
-        impl MyTrait for #ident {}
+use proc_macro::{self};
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Attribute, DeriveInput};
+
+fn group_attrs(
+    attrs: Vec<Attribute>,
+    accepted: &HashSet<String>,
+) -> Result<HashMap<String, Vec<TokenStream>>, syn::Error> {
+    let mut grouped_attrs: HashMap<String, Vec<TokenStream>> = HashMap::new();
+
+    for attr in attrs {
+        let segments = &attr.path.segments;
+
+        if segments.len() != 1 {
+            let ident = &segments.iter().nth(1).unwrap().ident;
+
+            return Err(syn::Error::new(
+                ident.span(),
+                "expected only one path segment",
+            ));
+        }
+
+        let ident = &segments.first().unwrap().ident;
+        let ident_str = ident.to_string();
+
+        if accepted.contains(&ident_str) {
+            grouped_attrs
+                .entry(ident_str)
+                .or_insert_with(Vec::new)
+                .extend(vec![attr.tokens]);
+        }
+    }
+
+    return Ok(grouped_attrs);
+}
+
+#[proc_macro_derive(RecursiveDescent, attributes(grammar, token))]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let DeriveInput { attrs, .. } = parse_macro_input!(input);
+
+    let mut set = HashSet::new();
+    set.insert("token".to_string());
+    set.insert("grammar".to_string());
+
+    let groups = match group_attrs(attrs, &set) {
+        Ok(g) => g,
+        Err(e) => return e.to_compile_error().into(),
     };
+
+    eprintln!("{:#?}", groups);
+
+    let output = quote! {};
     output.into()
 }
--- OxyMP/src/main.rs
+++ OxyMP/src/main.rs
@@ -1,14 +1,17 @@
-use oxymp::MyTrait;
+use oxymp::RecursiveDescent;
 
-trait MyTrait {
-    fn answer() -> i32 {
-        42
-    }
+fn match_number(matched: &str) -> i64 {
+    return matched.parse().unwrap();
 }
 
-#[derive(MyTrait)]
+#[derive(RecursiveDescent)]
+#[token("ParanLeft", "(")]
+#[token("ParanRight", ")")]
+#[token("NewLine", "\n")]
+#[token("Plus", "+")]
+#[token("Minus", "-")]
+#[token("Number", i64, ("/-?[0-9]+/", match_number))]
+#[grammar("expr -> Number (('+' | '-') expr)?")]
 struct Foo;
 
-fn main() {
-    println!("{}", Foo::answer());
-}
+fn main() {}
