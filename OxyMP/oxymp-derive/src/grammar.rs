c50e8b7f17229f27cd3e6323a7e4eeb31d6aeb21
Cozma Rares <cozmarares11@gmail.com>
2024-09-09 14:24:22 +0300

    fix(grammar): add error handling for unknown and shared token patterns

--- OxyMP/oxymp-derive/src/grammar.rs
+++ OxyMP/oxymp-derive/src/grammar.rs
@@ -10,7 +10,6 @@ use nom::{
     sequence::{delimited, tuple},
     IResult,
 };
-use proc_macro2::Span;
 
 use crate::lexer::TokenInfo;
 
@@ -39,30 +38,40 @@ enum NameType {
 }
 
 impl GrammarNode {
-    fn from_raw(node: RawGrammarNode, names: &HashMap<String, NameType>) -> Self {
+    fn from_raw(
+        node: RawGrammarNode,
+        names: &HashMap<String, NameType>,
+        patterns: &HashSet<String>,
+    ) -> Self {
         match node {
             RawGrammarNode::Name(name) => match names.get(&name) {
+                None => panic!("Error when parsing grammar rules\nUnknown name: {}", name),
                 Some(ty) => match ty {
                     NameType::Rule => GrammarNode::Rule(name),
                     NameType::Token => GrammarNode::Token(name),
                 },
-                None => panic!("Unknown name: {}", name),
             },
-            RawGrammarNode::Pattern(pattern) => GrammarNode::Pattern(pattern),
+            RawGrammarNode::Pattern(pattern) => match patterns.contains(&pattern) {
+                false => panic!(
+                    "Error when parsing grammar rules\nUnknown token pattern: '{}'",
+                    pattern
+                ),
+                true => GrammarNode::Pattern(pattern),
+            },
             RawGrammarNode::Expr(exprs) => GrammarNode::Expr(
                 exprs
                     .into_iter()
-                    .map(|e| GrammarNode::from_raw(e, names))
+                    .map(|e| GrammarNode::from_raw(e, names, patterns))
                     .collect(),
             ),
             RawGrammarNode::Choice(exprs) => GrammarNode::Choice(
                 exprs
                     .into_iter()
-                    .map(|e| GrammarNode::from_raw(e, names))
+                    .map(|e| GrammarNode::from_raw(e, names, patterns))
                     .collect(),
             ),
             RawGrammarNode::Optional(expr) => {
-                GrammarNode::Optional(Box::new(GrammarNode::from_raw(*expr, names)))
+                GrammarNode::Optional(Box::new(GrammarNode::from_raw(*expr, names, patterns)))
             }
         }
     }
@@ -83,35 +92,47 @@ pub fn aggragate_rules(
     token_info: &Vec<TokenInfo>,
 ) -> HashMap<String, GrammarNode> {
     let mut names = HashMap::new();
+    let mut patterns = HashSet::new();
 
     rules.iter().for_each(|rule| {
         names.insert(rule.name.clone(), NameType::Rule);
     });
 
-    token_info
-        .iter()
-        .filter(|info| match info {
-            TokenInfo::Exact(_) => true,
-            TokenInfo::Regex(_) => true,
-            _ => false,
-        })
-        .map(|info| match info {
-            TokenInfo::Exact(tok) => tok.name.clone(),
-            TokenInfo::Regex(tok) => tok.name.clone(),
-            _ => unreachable!(),
-        })
-        .for_each(|tok_name| {
-            if names.contains_key(&tok_name) {
-                panic!("A token and a rule share the same name: {}", tok_name)
-            }
+    let mut add_token_name = |tok_name| {
+        if names.contains_key(&tok_name) {
+            panic!(
+                "Error when parsing grammar rules\nA token and a rule share the same name: {}",
+                tok_name
+            )
+        }
 
-            names.insert(tok_name, NameType::Token);
-        });
+        names.insert(tok_name, NameType::Token);
+    };
+
+    let mut add_token_pattern = |tok_pattern| {
+        if patterns.contains(&tok_pattern) {
+            panic!(
+                "Error when parsing grammar rules\nTwo tokens share the same pattern: '{}'",
+                tok_pattern
+            )
+        }
+
+        patterns.insert(tok_pattern);
+    };
+
+    token_info.iter().for_each(|info| match info {
+        TokenInfo::Exact(tok) => {
+            add_token_name(tok.name.clone());
+            add_token_pattern(tok.pattern.clone())
+        }
+        TokenInfo::Regex(tok) => add_token_name(tok.name.clone()),
+        _ => {}
+    });
 
     rules
         .into_iter()
         .fold(HashMap::new(), |mut acc, RawGrammarRule { name, rule }| {
-            acc.insert(name, GrammarNode::from_raw(rule, &names));
+            acc.insert(name, GrammarNode::from_raw(rule, &names, &patterns));
             acc
         })
 }
