12eae77a8ce499acb766d0735454ff61d83e99a7
Cozma Rares <cozmarares11@gmail.com>
2024-10-31 14:10:52 +0200

    refactor(symbols): add definition cache

--- OxyMP/oxymp-derive/src/symbols.rs
+++ OxyMP/oxymp-derive/src/symbols.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use lazy_static::lazy_static;
 use quote::quote;
@@ -35,64 +35,63 @@ pub enum Symbol {
     Regex,
 }
 
-struct Definition {
-    path: Arc<[&'static str]>,
-    base: &'static str,
-}
-
-impl From<(&[&'static str], &'static str)> for Definition {
-    fn from(value: (&[&'static str], &'static str)) -> Definition {
-        let (path, base) = value;
-        let path = path.to_vec().into();
-        Definition { path, base }
-    }
-}
+type Definition<'a> = (&'a [&'a str], &'a str);
 
 #[rustfmt::skip]
 lazy_static! {
-    static ref STD_LIB: HashMap<Symbol, Definition> = {
+    static ref STD_LIB: HashMap<Symbol, Definition<'static>> = {
         let mut m = HashMap::new();
 
-        m.insert(Symbol::CoreStr,   (&["core", "primitive"][..], "str"  ).into());
-        m.insert(Symbol::CoreUsize, (&["core", "primitive"][..], "usize").into());
+        m.insert(Symbol::CoreStr,   (&["core", "primitive"][..], "str"  ));
+        m.insert(Symbol::CoreUsize, (&["core", "primitive"][..], "usize"));
 
-        m.insert(Symbol::DeriveDebug, (&["std", "fmt"  ][..], "Debug").into());
-        m.insert(Symbol::DeriveClone, (&["std", "clone"][..], "Clone").into());
+        m.insert(Symbol::DeriveDebug, (&["std", "fmt"  ][..], "Debug"));
+        m.insert(Symbol::DeriveClone, (&["std", "clone"][..], "Clone"));
 
-        m.insert(Symbol::Result, (&["std", "result"          ][..], "Result").into());
-        m.insert(Symbol::Ok,     (&["std", "result", "Result"][..], "Ok"    ).into());
-        m.insert(Symbol::Err,    (&["std", "result", "Result"][..], "Err"   ).into());
+        m.insert(Symbol::Result, (&["std", "result"          ][..], "Result"));
+        m.insert(Symbol::Ok,     (&["std", "result", "Result"][..], "Ok"    ));
+        m.insert(Symbol::Err,    (&["std", "result", "Result"][..], "Err"   ));
 
-        m.insert(Symbol::Option, (&["std", "option"          ][..], "Option").into());
-        m.insert(Symbol::None,   (&["std", "option", "Option"][..], "None"  ).into());
-        m.insert(Symbol::Some,   (&["std", "option", "Option"][..], "Some"  ).into());
+        m.insert(Symbol::Option, (&["std", "option"          ][..], "Option"));
+        m.insert(Symbol::None,   (&["std", "option", "Option"][..], "None"  ));
+        m.insert(Symbol::Some,   (&["std", "option", "Option"][..], "Some"  ));
 
-        m.insert(Symbol::String, (&["std", "string"][..], "String").into());
-        m.insert(Symbol::Box,    (&["std", "boxed" ][..], "Box"   ).into());
-        m.insert(Symbol::Vec,    (&["std", "vec"   ][..], "Vec"   ).into());
-        m.insert(Symbol::Rc,     (&["std", "rc"    ][..], "Rc"    ).into());
+        m.insert(Symbol::String, (&["std", "string"][..], "String"));
+        m.insert(Symbol::Box,    (&["std", "boxed" ][..], "Box"   ));
+        m.insert(Symbol::Vec,    (&["std", "vec"   ][..], "Vec"   ));
+        m.insert(Symbol::Rc,     (&["std", "rc"    ][..], "Rc"    ));
 
-        m.insert(Symbol::Fn,    (&["std", "ops"][..], "Fn"    ).into());
-        m.insert(Symbol::Deref, (&["std", "ops"][..], "Deref" ).into());
+        m.insert(Symbol::Fn,    (&["std", "ops"][..], "Fn"    ));
+        m.insert(Symbol::Deref, (&["std", "ops"][..], "Deref" ));
 
-        m.insert(Symbol::FormatMacro, (&["std"][..], "format").into());
-        m.insert(Symbol::VecMacro,    (&["std"][..], "vec")   .into());
+        m.insert(Symbol::FormatMacro, (&["std"][..], "format"));
+        m.insert(Symbol::VecMacro,    (&["std"][..], "vec")   );
 
-        m.insert(Symbol::Regex, (&["regex"][..], "Regex").into());
+        m.insert(Symbol::Regex, (&["regex"][..], "Regex"));
 
         m
     };
 }
 
-// TODO: add a cache
-pub fn get_def(symbol: Symbol, simple: bool) -> proc_macro2::TokenStream {
-    let def = STD_LIB.get(&symbol).unwrap();
-    let base = base_ident(def.base);
+thread_local! {
+    static SYMBOL_CACHE: RefCell<HashMap<Symbol, Rc<proc_macro2::TokenStream>>> = RefCell::new(HashMap::new());
+}
 
-    if simple {
+pub fn get_def(symbol: Symbol, simple: bool) -> Rc<proc_macro2::TokenStream> {
+    if let Some(cached) = SYMBOL_CACHE.with(|c| c.borrow().get(&symbol).cloned()) {
+        return cached;
+    }
+
+    let def = STD_LIB.get(&symbol).unwrap();
+    let base = base_ident(def.1);
+
+    let toks = Rc::new(if simple {
         quote! { #base }
     } else {
-        let path = def.path.iter().map(|s| base_ident(s));
+        let path = def.0.iter().map(|s| base_ident(s));
         quote! { ::#(#path)::*::#base }
-    }
+    });
+
+    SYMBOL_CACHE.with(|c| c.borrow_mut().insert(symbol, toks.clone()));
+    toks
 }
